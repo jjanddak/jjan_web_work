6051a5701a4240400d392705090b59e4a680fca6
jonghyeok <opplane@gmail.com>
2020-01-29 15:15:54 +0900

    UsersController.java - login2() added

--- Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
@@ -4,7 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsersController {
@@ -35,6 +38,36 @@ public class UsersController {
 		//view page의 정보를 리턴.
 		return "users/login";
 	}
+	
+	/*
+	 * @RequestParam 어노테이션은 전송되는 파라미터를 자동으로 추출할 때 사용.
+	 * 단, 지역 변수의 이름은 파라미터의 이름과 같아야 한다.
+	 * 
+	 */
+	@RequestMapping("/users/login2")
+	public ModelAndView login2(@RequestParam String id, @RequestParam String pwd
+					, HttpSession session, ModelAndView mView) {
+		
+		//유효한 정보인지 여부
+		boolean isValid=false;
+		if(id.equals("gura") && pwd.equals("123")) {
+			isValid=true;
+			session.setAttribute("id", id);
+		}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isValid", isValid);
+		
+		//view page정보도 담고
+		mView.setViewName("users/login");
+		
+		//리턴한다.
+		return mView;
+	}
+	
+	
+	
+	
+	
 }
 
 
--- Spring03_Basic/src/main/webapp/WEB-INF/views/users/loginform.jsp
+++ Spring03_Basic/src/main/webapp/WEB-INF/views/users/loginform.jsp
@@ -7,7 +7,17 @@
 <title>로그인폼</title>
 </head>
 <body>
+<h1>로그인폼 1</h1>
 <form action="login.do" method="post">
+	<label for="id">아이디</label>
+	<input type="text" name="id" id="id" />
+	<label for="pwd">비밀번호</label>
+	<input type="password" name="pwd" id="pwd" />
+	<button type="submit">로그인</button>
+</form>
+
+<h1>로그인폼 2</h1>
+<form action="login2.do" method="post">
 	<label for="id">아이디</label>
 	<input type="text" name="id" id="id" />
 	<label for="pwd">비밀번호</label>
