31d8c5d5482120580baceb5b1b2b825248e4414b
jonghyeok <opplane@gmail.com>
2020-02-04 10:39:04 +0900

    login_form.jsp Controller이용해서 구현

--- Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
@@ -2,6 +2,9 @@ package com.gura.spring05.users.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -49,6 +52,44 @@ public class UsersController {
 		
 		return mView;
 	}
+	
+	//로그인폼 요청 처리
+	@RequestMapping("/users/loginform")
+	public String loginform(HttpServletRequest request) {
+		
+		// "url" 이라는 파라미터가 넘어오는지 읽어와 본다.  
+		String url=request.getParameter("url");
+		if(url==null){//만일 없으면 
+			//로그인 성공후에 index.jsp 페이지로 보낼수 있도록 구성한다. 
+			url=request.getContextPath()+"/home.do";
+		}
+		
+		//쿠키읽어오기
+		Cookie[] cookies=request.getCookies();
+
+		//저장된 아이디 담기
+		String savedId="";//빈문자열을 초기값으로.
+		String savedPwd="";//빈문자열을 초기값으로.
+		
+		if(cookies!=null){
+			for(Cookie tmp:cookies){
+				if(tmp.getName().equals("savedId")){
+					savedId=tmp.getValue();
+				}else if(tmp.getName().equals("savedPwd")){
+					savedPwd=tmp.getValue();
+				}
+			}
+		}
+		//view page에서 필요한 정보 넘겨주기
+		request.setAttribute("url", url);
+		request.setAttribute("savedId", savedId);
+		request.setAttribute("savedPwd", savedPwd);
+				
+		return "users/loginform";
+	}
+	
+	
+	
 	
 	
 	
