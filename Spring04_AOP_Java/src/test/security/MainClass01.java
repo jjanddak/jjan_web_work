f66f9d63ffa4bdbac4776eadf7bea983fe5b589d
jonghyeok <opplane@gmail.com>
2020-02-04 09:52:18 +0900

    문자열을 암호화하는 예제 추가

---
+++ Spring04_AOP_Java/src/test/security/MainClass01.java
@@ -0,0 +1,20 @@
+package test.security;
+
+import java.util.Scanner;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("문자열 입력 : ");
+		String str=scan.nextLine();
+		//문자열을 암호화해주는 객체 생성
+		BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+		//입력한 문자열 암호화.
+		String result=encoder.encode(str);
+		System.out.println("입력한 문자 : "+str);
+		System.out.println("암호화된 문자 : "+result);
+		
+	}
+}
